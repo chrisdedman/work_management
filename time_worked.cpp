408b8da0ca5860a2796db39af8e906c22c8917a5
Chris Dedman-Rollet <61106361+chrisdedman@users.noreply.github.com>
2022-06-21 14:37:35 -0700

    update console colors

--- time_worked.cpp
+++ time_worked.cpp
@@ -3,6 +3,7 @@
 #include <iomanip>
 #include <vector>
 #include <string>
+
 using namespace std;
 
 struct MyTimeWorked
@@ -25,7 +26,7 @@ int main()
     fstream data;
     int option;
     cout << "\033[2J\033[1;1H"; // Clear the screen
-    cout << "\t\t====== WARRIOR-TORO WORK DONE ======\n"
+    cout << "\t\t\x1B[94m====== WARRIOR-TORO WORK DONE ======\033[0m\n"
          << endl;
     cout << "\t\t[1] Record Work Done | [2] Read Work Done" << endl;
     cin >> option;
@@ -48,11 +49,11 @@ int main()
 void getData(vector<MyTimeWorked> &vDB)
 {
     MyTimeWorked myWork;
-    cout << "Enter Work Information [xxx to end]" << endl;
+    cout << "Enter Work Information \x1B[31m[xxx to end]\033[0m" << endl;
     while (true)
     {
         cin.ignore();
-        cout << "Enter the date worked [mm/dd/yyyy]: ";
+        cout << "Enter the date worked \x1B[94m[mm/dd/yyyy]\033[0m: ";
         getline(cin, myWork.day);
         if (myWork.day == "xxx")
             break;
@@ -112,7 +113,7 @@ vector<MyTimeWorked> readData(fstream &data)
 void showDatabase(vector<MyTimeWorked> vData)
 {
     int totalWorked = 0;
-    cout << setw(15) << left << "DATE WORKED" << setw(15) << left << "HOURS WORKED" << setw(15) << left << "TASK PERFORMED" << endl;
+    cout << setw(15) << left << "\x1B[36mDATE WORKED\033[0m\t" << setw(15) << left << "\x1B[36mHOURS WORKED\033[0m\t" << setw(15) << left << "\x1B[36mTASK PERFORMED\033[0m\t" << endl;
     for (int index = 0; index < vData.size(); index++)
     {
         totalWorked += vData[index].hours;
@@ -132,14 +133,15 @@ void showDatabase(vector<MyTimeWorked> vData)
                  << setw(13) << left << (vData[index].hours % 60)
                  << setw(15) << left << vData[index].task << endl;
         }
+        
     }
-
+    
     if ((totalWorked % 60) == 0)
     {
-        cout << "\nTOTAL HOURS WORKED: " << (totalWorked / 60) << ":00" << endl;
+        cout << "\n\x1B[35mTOTAL HOURS WORKED:\033[0m " << (totalWorked / 60) << ":00" << endl;
     }
     else
     {
-        cout << "\nTOTAL HOURS WORKED: " << (totalWorked / 60) << ':' << (totalWorked % 60) << endl;
+        cout << "\n\x1B[35mTOTAL HOURS WORKED:\033[0m " << (totalWorked / 60) << ':' << (totalWorked % 60) << endl;
     }
 }
