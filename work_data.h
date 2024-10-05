e6746480aaae657b8b0ae5ecfe79860111ad266d
Chris <rolletchris1@gmail.com>
2024-10-05 13:36:45 -0700

    updated indentation

--- work_data.h
+++ work_data.h
@@ -5,28 +5,27 @@
 #include <string>
 #include <stdio.h>
 
-
 class MyWork
 {
-    private:
-        std::string day;
-        std::string task;
-        int hours;
-        
-    public:
-        int getHours() const {return hours; }
-        std::string getDay() const { return day; }
-        std::string getTask() const { return task; }
-        
-        void getData();
-        void setDay(std::string d) { day = d; }
-        void setTask(std::string t) { task = t; }
-        void setHours(int h) { hours = h; }
+private:
+    std::string day;
+    std::string task;
+    int hours;
 
-        std::fstream spreadsheet;
-        int totalHours;
-        void makeDatabase(std::fstream &spreadsheet);
-        void readData(std::fstream &spreadsheet);
-        void showDatabase();
-        void deleteDatabase();
+public:
+    int getHours() const { return hours; }
+    std::string getDay() const { return day; }
+    std::string getTask() const { return task; }
+
+    void getData();
+    void setDay(std::string d) { day = d; }
+    void setTask(std::string t) { task = t; }
+    void setHours(int h) { hours = h; }
+
+    std::fstream spreadsheet;
+    int totalHours;
+    void makeDatabase(std::fstream &spreadsheet);
+    void readData(std::fstream &spreadsheet);
+    void showDatabase();
+    void deleteDatabase();
 };
